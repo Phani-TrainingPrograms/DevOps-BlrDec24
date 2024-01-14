4fedd71c4b1bad515bb60a058d0f3cbfd75f2a58
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-01-14 14:23:02 +0530

    Add files via upload

---
+++ Microservices/SampleWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SampleWebApi.Data;
+using SampleWebApi.Models;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly OnlineShopDbContext _context;
+
+        public ProductsController(OnlineShopDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
---
+++ Microservices/SampleWebApi/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SampleWebApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<OnlineShopDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
